16f26846b853023d90702e66aa41cb50df7faa2f
borisngong <borisngong@gmail>
2024-02-29 15:58:45 -0800

    A function that finds the sibling of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,42 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - responsible for finding the sibling of a node
- * in a binary tree.
- * @node: Represents pointer to the node to find the sibling of
- * Return: A pointer to the sibling node, or NULL if node is NULL,
+ * binary_tree_sibling - Responsible for finding the sibling of a node.
+ * @node: Represents a pointer to the node to find the sibling of.
+ * Return: Represents a pointer to the sibling node, or NULL if node is NULL,
  * the parent is NULL, or node has no sibling.
  */
-
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	return (find_sibling(node->parent, node));
-}
+	if (node->parent->left == node)
+		return (node->parent->right);
 
-/**
- * find_sibling - Responsible for recursively finding the sibling of a node.
- * @parent: represents the parent node to search for the sibling.
- * @node: represents the node for which to find the sibling.
- * Return: A pointer to the sibling node, or NULL if not found.
- */
-binary_tree_t *find_sibling(binary_tree_t *parent, binary_tree_t *node)
-{
-	binary_tree_t *sibling = 0;
-
-	if (parent == NULL || parent->left == NULL || parent->right == NULL)
-		return (NULL);
-
-	if (parent->left == node)
-		return (parent->right);
-	else if (parent->right == node)
-		return (parent->left);
-
-	sibling = find_sibling(parent->left, node);
-	if (sibling == NULL)
-		sibling = find_sibling(parent->right, node);
-
-	return (sibling);
+	return (node->parent->left);
 }
