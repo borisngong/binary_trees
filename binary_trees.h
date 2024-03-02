cada86a9d546de9eca2851e959178288f09d2920
borisngong <borisngong@gmail>
2024-03-01 16:01:53 -0800

    Binary Trees update

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,20 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/**
+ * struct levelorder_queue_s - Responsible for Level order traversal queue.
+ * @node: Represents a node of a binary tree.
+ * @next: Represents the next node to traverse to in the binary tree.
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
 
 /* HELPER FUNCTIONS */
 void binary_tree_print(const binary_tree_t *);
@@ -28,6 +42,7 @@ unsigned char bt_is_leaf_helper(const binary_tree_t *node);
 const binary_tree_t *bt_get_leaf_helper(const binary_tree_t *tree);
 int bt_is_perfect_recursive_helper(const binary_tree_t *tree,
                                    size_t leaf_depth, size_t level);
+
 
 /* PROTOTYPES */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -47,5 +62,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
